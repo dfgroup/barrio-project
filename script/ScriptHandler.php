53702aa26170588dbefe2518269e66365a6d21c5
hatuhay <alberto@siles.pe>
2016-12-09 15:41:08 -0500

    Create script directory

---
+++ script/ScriptHandler.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Contains \DFGroup\Barrio\ScriptHandler.
+ */
+
+namespace DFGroup\Barrio;
+
+use Composer\Script\Event;
+use Composer\Util\ProcessExecutor;
+
+class ScriptHandler {
+
+  /**
+   * Moves front-end libraries to Barrio's installed directory.
+   *
+   * @param \Composer\Script\Event $event
+   *   The script event.
+   */
+  public static function deployLibraries(Event $event) {
+    $extra = $event->getComposer()->getPackage()->getExtra();
+
+    if (isset($extra['installer-paths'])) {
+      foreach ($extra['installer-paths'] as $path => $criteria) {
+        if (array_intersect(['drupal/barrio', 'type:drupal-profile'], $criteria)) {
+          $barrio = $path;
+        }
+      }
+      if (isset($barrio)) {
+        $barrio = str_replace('{$name}', 'barrio', $lightning);
+
+        $executor = new ProcessExecutor($event->getIO());
+        $output = NULL;
+        $executor->execute('npm run install-libraries', $output, $barrio);
+      }
+    }
+  }
+
+}
